988d0542fea2ae9141d9ad5b921f60250043d6ab
camilalopez09 <camilalovevas@gmail.com>
2023-08-27 21:08:49 -0500

    Commit 1 Cami Prueba
    Prueba de creación de index

---
+++ Index/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FirstApp from './FirstApp';
+
+
+
+
+//Si un valor lo estás mando en js pero en html no se mueve entonces toca cambiar en el index.//
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <FirstApp value={5}/> 
+  </React.StrictMode>
+);
+
